99bb4a669bcbc52c476c8ab83eb95e8adb067229
Riscait <mr@nerco.jp>
2019-03-23 15:42:07 +0900

    Xcodeプロジェクトを作成しました。

---
+++ Stepippo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Stepippo
+//
+//  Created by 村松龍之介 on 2019/03/23.
+//  Copyright © 2019 Yasasii-kai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+

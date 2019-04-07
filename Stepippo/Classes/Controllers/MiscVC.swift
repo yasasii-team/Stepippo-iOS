7427ca6794e3518ef3b57ea39fb503445f4eab53
Riscait <mr@nerco.jp>
2019-04-07 23:53:57 +0900

    その他(Misc)画面の実装に必要な画面とコントローラーファイルを新規作成しました

---
+++ Stepippo/Classes/Controllers/MiscVC.swift
@@ -0,0 +1,17 @@
+//
+//  MiscVC.swift
+//  Stepippo
+//
+//  Created by 村松龍之介 on 2019/04/07.
+//  Copyright © 2019 Yasasii-kai. All rights reserved.
+//
+
+import UIKit
+
+final class MiscVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}

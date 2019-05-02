9a32f8898aab308c7d6727e0f417735bab5ed3af
Ayana Kato <ayana20130321@gmail.com>
2019-05-02 13:44:46 +0900

    進捗画面のstoryboardとUIViewControllerファイルを作成

---
+++ Stepippo/Classes/Controllers/ProgVC.swift
@@ -0,0 +1,30 @@
+//
+//  ProgVC.swift
+//  Stepippo
+//
+//  Created by 加藤彩那 on 5/2/31 H.
+//  Copyright © 31 Heisei Yasasii-kai. All rights reserved.
+//
+
+import UIKit
+
+class ProgVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

25d30dcf4cdf2b883b0890c5810322e4d9d1a741
naipaka <naipakapaka@gmail.com>
2019-05-18 20:20:13 +0900

    IPPOのデータモデルを作成

---
+++ Stepippo/Classes/Models/IPPO.swift
@@ -0,0 +1,28 @@
+//
+//  IPPO.swift
+//  Stepippo
+//
+//  Created by ないぱか on 2019/05/18.
+//  Copyright © 2019 Yasasii-kai. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class IPPO: Object {
+    // 一意のID
+    dynamic var primaryKey = 0
+    
+    // タスクの名前
+    dynamic var title = ""
+    
+    // 進捗状態
+    // 後でやる："stock", 挑戦中："challenging", 達成見込："performed", 達成済み："achieved"
+    dynamic var status = ""
+    
+    // タスク追加日時
+    dynamic var addDateTime: Date?
+    
+    // タスク実施日時
+    dynamic var performedDateTime: Date?
+}

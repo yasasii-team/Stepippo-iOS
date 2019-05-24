300c85c0537c1716fdf849a4176dba07aba72cac
Ayana Kato <ayana20130321@gmail.com>
2019-05-24 19:32:58 +0900

    checkButtonの実装

--- Stepippo/Classes/Views/CheckButton.swift
+++ Stepippo/Classes/Views/CheckButton.swift
@@ -9,13 +9,30 @@
 import UIKit
 
 class CheckButton: UIButton {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    // Images
+    let checkedImage = UIImage(named: "Checkbox")! as UIImage
+    let uncheckedImage = UIImage(named: "UnCheckbox")! as UIImage
+        
+    // Bool property
+    var isChecked: Bool = false {
+        didSet{
+            if isChecked == true {
+                self.setImage(checkedImage, for: UIControl.State.normal)
+            } else {
+                self.setImage(uncheckedImage, for: UIControl.State.normal)
+            }
+        }
     }
-    */
-
+    
+    override func awakeFromNib() {
+        self.addTarget(self, action:#selector(buttonClicked(sender:)), for:UIControl.Event.touchUpInside)
+        self.isChecked = false
+    }
+    
+    
+    @objc func buttonClicked(sender: UIButton) {
+        if sender == self {
+            isChecked = !isChecked
+        }
+    }
 }

b93f2fa96b2813b073827d7e596150f8295177d2
eyener3 <ayana20130321@gmail.com>
2019-06-14 04:42:41 +0900

    現在日時を取得して残り日数を算出するUserDefaultsの実装

--- Stepippo/Classes/Controllers/ProgVC.swift
+++ Stepippo/Classes/Controllers/ProgVC.swift
@@ -64,6 +64,59 @@ final class ProgVC: UIViewController {
         // 選択した期間を「deadlineCycle」という名前(key)でUserDefaultsに保存する
         userDefaults.set(sender.title, forKey: "deadlineCycle")
     }
+
+    
+    func calcRemainingPeriod() -> String {
+        // 現在時刻を取得
+        let today = Date()
+        print("現在日時: \(today)")
+        // UserDefaultsのインスタンス
+        let userDefaults = UserDefaults.standard
+        // カレンダーのインスタンス
+        let calendar = Calendar.current
+        // 条件として、UserDefaultsに保存した期日サイクルを取得して使う
+        switch userDefaults.string(forKey: "deadlineCycle") {
+            
+        case "毎日":
+            let timeOfDayToStart = userDefaults.integer(forKey: "timeOfDayToStart") + 1
+            // 期日を計算
+            let nextPeriod = calendar.date(bySetting: .minute, value: timeOfDayToStart, of: today)
+            // 残り時間を計算
+            let remaining = calendar.dateComponents([.minute], from: today, to: nextPeriod!)
+            print("期日まで残り\(remaining.minute!)分")
+            
+            // TODO: 毎日の場合の計算
+            return "期日まで残り\(remaining.minute!)分"
+            
+        case "毎週":
+            // UserDefaultsで保存してある曜日設定を取得
+            let dayOfWeekToStart = userDefaults.integer(forKey: "dayOfWeekToStart") + 1
+            // 期日を計算
+            let nextPeriod = calendar.date(bySetting: .weekday, value: dayOfWeekToStart, of: today)
+            print("期日: ", nextPeriod ?? "nil")
+            // 残り日数を計算
+            let remaining = calendar.dateComponents([.day, .hour], from: today, to: nextPeriod!)
+            print("期日まで残り\(remaining.day!)日と\(remaining.hour!)時間")
+            
+            return "残り\(remaining.day!)日と\(remaining.hour!)時間"
+            
+        case "毎月":
+            // UserDefaultsで保存してある曜日設定を取得
+            let dayOfMonthToStart = userDefaults.integer(forKey: "dayOfMonthToStart") + 1
+            // 期日を計算
+            let nextPeriod = calendar.date(bySetting: .day, value: dayOfMonthToStart, of: today)
+            // 残り日数を計算
+            let remaining = calendar.dateComponents([.day], from: today, to: nextPeriod!)
+            print("期日まで残り\(remaining.day!)日")
+            
+            // TODO: 毎月の場合の計算
+            return "残り\(remaining.day!)日"
+            
+        default:
+            // TODO: 定数化してdefaultsを使わない実装にする
+            return "TODO: default"
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -72,5 +125,8 @@ final class ProgVC: UIViewController {
         // UserDefaultsに今保存した値を確認する
         let nowDeadlineCycle = userDefaults.string(forKey: "deadlineCycle") ?? "deadlineCycleは保存されていません"
         print("期間設定: \(nowDeadlineCycle)")
+        let result = calcRemainingPeriod()
+        print(result)
+        
     }
 }

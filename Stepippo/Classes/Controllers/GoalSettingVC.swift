bd11de1192d4e4ca44bd807afb0826d0d6398d13
izumi_yoshiki <izumiyoshiki@icloud.com>
2019-06-24 01:04:53 +0900

    同じタイトルじゃない場合IPPOを登録

--- Stepippo/Classes/Controllers/GoalSettingVC.swift
+++ Stepippo/Classes/Controllers/GoalSettingVC.swift
@@ -7,10 +7,11 @@
 //
 
 import UIKit
+import RealmSwift
 /// タスクの目標を設定する画面
 final class GoalSettingVC: UIViewController, RealmObjectAccessible {
     var taskArray: [String] = []
-
+    let realm = try! Realm()
     // MARK: - IBOutlet properties
     @IBOutlet private weak var addTaskTextField: UITextField!
     @IBOutlet private weak var addTaskTableView: UITableView!
@@ -23,14 +24,14 @@ final class GoalSettingVC: UIViewController, RealmObjectAccessible {
     
     @IBAction func stockButton(_ sender: Any) {
         
-        if let addTaskText = addTaskTextField.text, !addTaskTextField.text!.isEmpty {
+        if let addTaskText = addTaskTextField.text, !addTaskText.isEmpty {
             let ippo = IPPO()
             ippo.title = addTaskText
             ippo.status = .stock
             write(ippo)
         } else {
-            let alert: UIAlertController = UIAlertController(title: "アラート表示", message: "タスク名が空です", preferredStyle:  UIAlertController.Style.alert)
-            let cancelAction: UIAlertAction = UIAlertAction(title: "タスク名を入力する", style: UIAlertAction.Style.cancel, handler:{
+            let alert = UIAlertController(title: "アラート表示", message: "タスク名が空です", preferredStyle: .alert)
+            let cancelAction = UIAlertAction(title: "タスク名を入力する", style: .cancel, handler:{
                 (action: UIAlertAction!) -> Void in
                 print("Cancel")
             })
@@ -52,16 +53,46 @@ final class GoalSettingVC: UIViewController, RealmObjectAccessible {
             taskArray.append(task)
             addTaskTableView.reloadData()
             addTaskTextField.text = ""
+        } else {
+            let alert = UIAlertController(title: "アラート表示", message: "IPPOは最大３つまでの登録です。", preferredStyle: .alert)
+            let cancelAction = UIAlertAction(title: "キャンセル", style: .cancel, handler:{
+                (action: UIAlertAction!) -> Void in
+                print("Cancel")
+            })
+            alert.addAction(cancelAction)
+            present(alert, animated: true, completion: nil)
         }
     }
     
     @IBAction func doneButton(_ sender: Any) {
-        let ippo = IPPO()
-        ippo.title = taskArray.popLast()!
-        ippo.status = .achieved
-        ippo.performedDateTime = Date()
-        write(ippo)
+        let ippoList = fetch(IPPO.self, predicate: NSPredicate(format: "_status = %@", argumentArray: [IPPOStatus.challenging.rawValue, IPPOStatus.performed.rawValue]))
+
+        var sameTitle:[Int] = []
+        for _ in 0..<taskArray.count {
+            sameTitle.append(0)
+        }
+        
+        var i = 0
+        for task in taskArray {
+            for ippo in ippoList {
+                if ippo.title == task {
+                    sameTitle[i] += 1
+                }
+            }
+            i += 1
+        }
+        i = 0
+        for title in sameTitle {
+            if title == 0 {//同じタイトルじゃない場合IPPOを登録
+                let ippo = IPPO()
+                ippo.title = taskArray[i]
+                ippo.status = .challenging
+                write(ippo)
+            }
+            i += 1
+        }
     }
+    
     // MARK: - Life cycle methods
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -69,6 +100,15 @@ final class GoalSettingVC: UIViewController, RealmObjectAccessible {
         addTaskTableView.delegate = self
         addTaskTableView.dataSource = self
         addTaskTableView.register(UINib(nibName: "AddTaskCell", bundle: nil), forCellReuseIdentifier: "AddTaskCell")
+        
+        let ippoList = fetch(IPPO.self, predicate: NSPredicate(format: "_status = %@", argumentArray: [IPPOStatus.challenging.rawValue, IPPOStatus.performed.rawValue]))
+        for ippo in ippoList {
+            if tableViewHeight.constant < 150 {
+                tableViewHeight.constant += 50
+            }
+            taskArray.append(ippo.title)
+        }
+        addTaskTableView.reloadData()
     }
 }
 
@@ -107,6 +147,16 @@ extension GoalSettingVC: UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
+            
+            let ippoList = fetch(IPPO.self, predicate: NSPredicate(format: "_status = %@", argumentArray: [IPPOStatus.challenging.rawValue, IPPOStatus.performed.rawValue]))
+            for ippo in ippoList {
+                if ippo.title == taskArray[indexPath.row] {
+                    try! realm.write {
+                        realm.delete(ippo)
+                    }
+                }
+            }
+            
             taskArray.remove(at: indexPath.row)
             addTaskTableView.deleteRows(at: [indexPath], with: .fade)
             tableViewHeight.constant -= 50
